559a1f68c91057adeff17a64e79565fa2bf3b527
aizotovvv <164581097+aizotovvv@users.noreply.github.com>
2026-09-15 23:52:45 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import asyncio
 import logging
 from mcstatus import MinecraftServer
